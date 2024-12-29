1c59e682c940f90bfaf3563ff329472c2a3ed403
Jay Singh <jaysinghmd@gmail.com>
2024-12-30 00:51:08 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,97 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks'))
+
+    if (storedTasks) {
+        storedTasks.forEach((tasks) => tasks.push(task))
+        updateTaskList();
+        updateStats();
+
+    }
+})
+let tasks = [];
+
+const saveTasks = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+const addTask = () => {
+    const taskInput = document.getElementById('taskInput')
+    const text = taskInput.value.trim();
+
+    if (text) {
+        tasks.push({ text: text, completed: false });
+        taskInput.value = "";
+        updateTasksList();
+        updateStats();
+        saveTasks();
+
+    }
+};
+
+const toggleTaskComplete = (index) => {
+    tasks[index].completed = !tasks[index].completed;
+    updateTaskList();
+    updateStats();
+    saveTasks();
+};
+
+const deleteTask = (index) => {
+    tasks.splice(index, 1);
+    updateTaskList();
+    updateStats();
+    saveTasks();
+};
+
+const editTask = (index) => {
+    const taskInput = document.getElementById('taskInput');
+    taskInput.value = tasks[index].text;
+
+    tasks.splice(index, 1);
+    updateTaskList();
+    updateStats();
+    saveTasks();
+};
+
+const updateStats = () => {
+    const conpletedTasks = tasks.filter((tasks) => tasks.completed).length;
+    const totalTasks = tasks.length;
+    const progress = (completeTasks / totalTasks) * 100;
+
+    const progressBar = document.getElementById("progress");
+
+    progressBar.style.widows = `${progress}%`;
+
+    document.getElementById("numbers").innerText = `${completeTasks}/ ${totalTasks}`;
+};
+
+const updateTasksList = () => {
+    const taskList = document.getElementById('task-list');
+    taskList.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        const listItem = document.createElement("li");
+
+        listItem.innerHTML = `
+        <div class="taskItem">
+            <div class="task ${task.completed ? 'completed' : ''}">
+                <input type="checkbox" class="checkbox" ${task.completed ? "checked" : ""}/>
+                <p>${task.text} </p>
+            </div>
+
+            <div class="icons">
+                <img src="edit.png" onClick = "editTask(${index})" />
+                <img src="bin.png" onClick = "deleteTask(${index})" />
+            </div>
+        </div>
+
+        `;
+        listItem.addEventListener('change', () => toggleTaskComplete(index));
+        taskList.append(listItem);
+    })
+}
+
+document.getElementById('newTask').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    addTask();
+})
